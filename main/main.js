da6a7a928dd95212edaef25a9210cedfc9969fea
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-19 12:51:38 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,12 @@
 module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+    var num_list=['._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._. ','|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_| ','|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..| '];
+    var list='';
+    for(let i=0;i<3;i++){
+        for(let j=0;j<nums.length;j++){
+            let num=parseInt(nums.charAt(j));
+            list=list+num_list[i].substring(num*6,num*6+4);
+        }
+        list=list.substring(0,list.length-1)+'\n';
+    }
+    return list;
 };
